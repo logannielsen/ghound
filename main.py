aef711e4922490411d72088b6643c5d7f1c9798d
Logan <logannielsen@live.com>
2020-01-20 19:07:48 +1100

    querying updates

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
                 race_card_list = MeetRaces(read_file(meeting.file_path())).races_id_list
                 for race_id in race_card_list:
                     if race_id != "0":
-                        race_path = build_and_check_path(race_id, 
+                        race_path = build_and_check_path(race_id,
                                             meeting.track,
                                             root="sample_races")
                         race_url = f"https://fasttrack.grv.org.au/RaceField/ViewRaces/{meeting.meeting_id}?raceId={race_id}"
@@ -33,49 +33,41 @@ if __name__ == "__main__":
                         # print(race_id, races.number, races.time, races.date, races.first_prize,
                         # races.second_prize, races.third_prize, races.distance, races.race_type,
                         # races.quinella_price, races.exacta_price, races.trifecta_price, races.first4_price)
-                        print("*"*100)
+                        # print("*"*100)
                         greyhounds = races.get_greys()
-                        print(greyhounds)
                         for hound in greyhounds:
+                            print(hound.name)
                             hound.save_and_return_greyhound_form(req_session)
                             q = sess.query(GreyhoundTable.id).filter(GreyhoundTable.id==hound.id).one_or_none()
-                            print(hound.name, hound.id)
                             if not q:
+                                print(hound.name)
                                 ghound = GreyhoundTable(id=hound.id, name=hound.name)
                                 sess.add(ghound)
                                 sess.flush()
-                                print(hound.name, hound.id)
                             with open(f"{hound.csv_path()}.csv", "r", encoding='utf-8') as f:
                                 form = GreyhoundForm(f)
                                 for race in form.race():
+                                    # print("here")
                                     if race['Pl'].isdigit():
-                                        print(race['Pl'])
-                                        print(race['Trk'])
-                                        print(hound.id)
                                         q2 = sess.query(TrackTable).filter(TrackTable.track_name==race['Trk'].strip()).one_or_none()
-                                        print(q2)
                                         if not q2:
                                             new_track = TrackTable(track_name=race['Trk'])
                                             sess.add(new_track)
                                             sess.flush()
                                         xy = sess.query(TrackTable).filter(TrackTable.track_name==race['Trk'].strip()).one_or_none()
-                                        if xy.id:
-                                            print(f'here {xy.id}')
-                                            q3 = sess.query(RaceTable, TrackTable).filter(TrackTable.id==xy.id).\
+                                        q3 = sess.query(RaceTable).join(TrackTable).filter(TrackTable.id==xy.id).\
                                                                             filter(RaceTable.date==datetime.strptime(race['Date'], '%d/%m/%Y')).\
                                                                             filter(RaceTable.race_number == int(search(r'\d+', race['Race']).group(0))).one_or_none()
-                                            if not q3:
-                                                print(f'here now {xy.id}')
-                                                new_race = RaceTable(track_id=xy.id, date=datetime.strptime(race['Date'], '%d/%m/%Y'), race_number=int(search(r'\d+', race['Race']).group(0)))
-                                                sess.add(new_race)
-                                                sess.flush()
-                                                sess.commit()
-                                                print(xy.id)
-                                                gq = sess.query(RaceTable).filter(RaceTable.track_id==xy.id).\
+
+                                        if not q3:
+                                            new_race = RaceTable(track_id=xy.id, date=datetime.strptime(race['Date'], '%d/%m/%Y'), race_number=int(search(r'\d+', race['Race']).group(0)))
+                                            sess.add(new_race)
+                                            sess.flush()
+                                            sess.commit()
+                                        gq = sess.query(RaceTable).filter(RaceTable.track_id==xy.id).\
                                                                             filter(RaceTable.date==datetime.strptime(race['Date'], '%d/%m/%Y')).\
                                                                             filter(RaceTable.race_number == int(search(r'\d+', race['Race']).group(0))).one_or_none()
-                                                print(gq)
-                                                race_stats = RaceStatsTable(greyhound_id=hound.id,
+                                        race_stats = RaceStatsTable(greyhound_id=hound.id,
                                                                 race = gq.id,
                                                                 distance = int(race['Dist']),
                                                                 weight = race['Wght'],
@@ -89,11 +81,12 @@ if __name__ == "__main__":
                                                                 sp = race['S/P'],
                                                                 hcap = race['Hcap']
                                     )
-                                                sess.add(race_stats)
-                                                sess.flush()
-                                                sess.commit()
-    
-                            
-                            
-                            
-                        
+                                        sess.add(race_stats)
+                                        sess.flush()
+                                        sess.commit()
+                    print("race")
+            print("end loop")
+
+
+
+
--- test_sql_orm.py
+++ test_sql_orm.py
@@ -17,10 +17,20 @@ def test():
     for a in sess.query(GreyhoundTable).filter(TrackTable.id==6).all():
         print(a.id, a.name)
         print('done')
-    
+
 def test_a():
-    for dog, track in sess.query(GreyhoundTable, TrackTable).join(RaceStatsTable).join(RaceTable).join(TrackTable).filter(TrackTable.id=='9').all():
-        print(dog.id, dog.name, track.track_name)
+    dogs = sess.query(GreyhoundTable).join(RaceStatsTable, GreyhoundTable.id==RaceStatsTable.greyhound_id).join(RaceTable, RaceTable.id==RaceStatsTable.race).\
+                join(TrackTable, RaceTable.id==TrackTable.id).\
+                filter(RaceTable.date==datetime.strptime("15/12/2019", '%d/%m/%Y')).\
+                filter(TrackTable.track_name=='SAP').\
+                filter(RaceTable.race_number==1)
+    print(dogs.all())
+    print('done')
+
+def test_b():
+    for race in sess.query(RaceStatsTable).join(GreyhoundTable).join(RaceTable).join(TrackTable).filter(GreyhoundTable.name=="ASTON SPINEL").all():
+        print(race.id, race.distance)
         print('done')
 
 test_a()
+datetime.strptime("15/12/2019", '%d/%m/%Y')
